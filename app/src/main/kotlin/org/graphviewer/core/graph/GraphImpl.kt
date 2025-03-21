c100a6ef0c0c52ff22fda336c5ae1028db1d115e
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-21 17:51:46 +0100

    feat: add Graph factory

--- app/src/main/kotlin/org/graphviewer/core/graph/GraphImpl.kt
+++ app/src/main/kotlin/org/graphviewer/core/graph/GraphImpl.kt
@@ -1,6 +1,17 @@
 package org.graphviewer.core.graph
 
 class GraphImpl : Graph {
+    companion object {
+        fun create(edges: List<String>): Graph {
+            val graph = GraphImpl()
+            edges.forEach { line ->
+                val (v1, v2) = line.split("->")
+                graph.addEdge(VertexImpl(v1), VertexImpl(v2))
+            }
+            return graph
+        }
+    }
+
     private val adjacentList = mutableMapOf<Vertex, MutableList<Edge>>()
     private val disabledVertices = mutableSetOf<Vertex>()
 
