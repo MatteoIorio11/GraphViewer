3aa9d73b7a018c59a51449a12a5ec3d97851e2ed
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-22 10:00:36 +0100

    test: add new graph tests

--- app/src/test/kotlin/org/graphviewer/graph/GraphTest.kt
+++ app/src/test/kotlin/org/graphviewer/graph/GraphTest.kt
@@ -1,4 +1,18 @@
 package org.graphviewer.graph
 
-class GraphTest {
-}
+import io.kotest.core.spec.style.DescribeSpec
+import org.graphviewer.core.graph.GraphImpl
+import org.junit.jupiter.api.assertThrows
+
+class GraphTest :
+    DescribeSpec({
+        describe("Given as input a list of edges It should be possible to create a graph") {
+            val graph = GraphImpl.create(listOf("A->B", "B->C"))
+            assert(graph.dump().size == 2)
+        }
+        describe("Given as input a list of edges that It is not well formatted It should throw an exception") {
+            assertThrows<java.lang.IllegalArgumentException> {
+                GraphImpl.create(listOf("A->B", "B"))
+            }
+        }
+    })
