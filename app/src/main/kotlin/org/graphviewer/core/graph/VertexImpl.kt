c1220c54dddaedc7f49ec02c8538c21a695ab4e5
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-22 10:13:34 +0100

    feat: override equals, hashcode and tostring

--- app/src/main/kotlin/org/graphviewer/core/graph/VertexImpl.kt
+++ app/src/main/kotlin/org/graphviewer/core/graph/VertexImpl.kt
@@ -1,7 +1,19 @@
 package org.graphviewer.core.graph
 
+import com.google.common.base.Objects
+
 class VertexImpl(
     private val id: String,
 ) : Vertex {
     override fun getId(): String = id
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        val otherVertex = other as? VertexImpl ?: return false
+        return id == otherVertex.id
+    }
+
+    override fun toString(): String = "Vertex: $id"
+
+    override fun hashCode(): Int = Objects.hashCode(getId())
 }
