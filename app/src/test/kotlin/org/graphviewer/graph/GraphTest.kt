4d68e9889d1f6c66ac78d1cc74d0e99c8b0844a0
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-27 17:28:30 +0100

    test: add new test, avoid empty lines and consider only lines with content. Test create graph and validate input lines

--- app/src/test/kotlin/org/graphviewer/graph/GraphTest.kt
+++ app/src/test/kotlin/org/graphviewer/graph/GraphTest.kt
@@ -1,13 +1,16 @@
 package org.graphviewer.graph
 
 import io.kotest.core.spec.style.DescribeSpec
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.graphviewer.core.graph.GraphImpl
+import org.graphviewer.core.graph.GraphUtils
 import org.graphviewer.core.graph.VertexImpl
 import org.junit.jupiter.api.assertThrows
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class GraphTest :
     DescribeSpec({
         describe("Given as input a list of edges It should be possible to create a graph") {
@@ -57,7 +60,7 @@ class GraphTest :
                 @enduml
                 """.trimIndent()
             val graph = GraphImpl.create(listOf("a->b", "b->c"))
-            assertEquals(expected, GraphImpl.toPlantUml(graph))
+            assertEquals(expected, GraphUtils.toPlantUml(graph))
         }
         describe("Given a graph as input, If we disable a vertex It should not be present inside the plantuml representation") {
             val expected =
@@ -70,22 +73,32 @@ class GraphTest :
                 """.trimIndent()
             val graph = GraphImpl.create(listOf("a->b", "b->c", "a->c"))
             graph.disableVertex(VertexImpl("c"))
-            assertEquals(expected, GraphImpl.toPlantUml(graph))
+            assertEquals(expected, GraphUtils.toPlantUml(graph))
+        }
+        describe("Given as input a list of edges, where some of the lines are empty, It should be possible to create a graph") {
+            val graph = GraphImpl.create(listOf("A->B", "", "B->C"))
+            assert(graph.dump().size == 2)
+        }
+        describe("Given as input a list of edges, where there is an empty line, the validation check should return true") {
+            val validation = GraphUtils.isValidFormat(listOf("a->b", "", "b->c"))
+            validation.invokeOnCompletion {
+                assertTrue { validation.getCompleted() }
+            }
         }
         describe("Given as input a list of edges, If we check It, and It is well formatted It should return true") {
-            val validation = GraphImpl.isValidFormat(listOf("a->b", "b->c"))
+            val validation = GraphUtils.isValidFormat(listOf("a->b", "b->c"))
             validation.invokeOnCompletion {
                 assertTrue { validation.getCompleted() }
             }
         }
         describe("Given as input a list of edges that It is not well formatted, the validation check should return false") {
-            val validation = GraphImpl.isValidFormat(listOf("a->b", "b"))
+            val validation = GraphUtils.isValidFormat(listOf("a->b", "b"))
             validation.invokeOnCompletion {
                 assertFalse { validation.getCompleted() }
             }
         }
         describe("Given as input an empty list of edges, the validation check should return false") {
-            val validation = GraphImpl.isValidFormat(listOf())
+            val validation = GraphUtils.isValidFormat(listOf())
             validation.invokeOnCompletion {
                 assertFalse { validation.getCompleted() }
             }
